501c22d2be36f3f4f4aa86d8f70ae8fb77364210
sjcydev <sjcydev12@gmail.com>
2023-07-15 19:57:05 +0930

    Add email to admin

--- src/routes/api/emails/facturar/+server.ts
+++ src/routes/api/emails/facturar/+server.ts
@@ -47,6 +47,9 @@ export const POST = async ({ request }: RequestEvent) => {
           {
             email: cliente.correo,
           },
+          {
+            email: "xcargoservices68@gmail.com",
+          },
         ],
         subject: "Tienes paquetes listo para retirar!",
         htmlContent: html,
